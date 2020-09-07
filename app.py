93a1232e6b7447e67ab52795e563587bf35ee4de
DavidHZama <davidh.zama@gmail.com>
2020-09-07 13:36:43 -0500

    Fixed db import error

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import  Api
 from flask_jwt import JWT
 from security import authenticate,identity
-from db import db
 
 from resources.item import Item ,ItemList
 from resources.user import UserRegister
@@ -13,10 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'david'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) #Endpoint /auth
 
